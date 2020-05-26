ebc1be4ffebe02a36a6921c0e3a5d686c0a50a01
Raschid J.F. Rafaelly <me@raschidjfr.dev>
2020-05-26 16:40:51 -0500

    feat(login): remove 'forgot password' and 'register' options

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -1,12 +1,12 @@
 import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
-  {
-    title: 'Puzzles',
-    icon: 'question-mark-outline',
-    link: '/puzzles',
-    home: true,
-  },
+  // {
+  //   title: 'Puzzles',
+  //   icon: 'question-mark-outline',
+  //   link: '/puzzles',
+  //   home: true,
+  // },
   {
     title: 'Log In',
     icon: 'person-outline',
