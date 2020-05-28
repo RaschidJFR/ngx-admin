b6f7906bbdd67b391763de114f9ffc3a4c607a15
Raschid J.F. Rafaelly <me@raschidjfr.dev>
2020-05-28 18:29:12 -0500

    fix(auth.service): it was not possible to log out from invalid session

--- src/app/@core/auth/auth.service.ts
+++ src/app/@core/auth/auth.service.ts
@@ -2,9 +2,10 @@ import { Injectable, Inject } from '@angular/core';
 import { NbAuthService, NbTokenService, NB_AUTH_STRATEGIES, NbAuthResult } from '@nebular/auth';
 import * as Parse from 'parse';
 import { ParseService } from '../parse/parse.service';
-import { take } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { take, catchError } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 import { Router } from '@angular/router';
+import { ParseAuthStrategy } from './auth-strategy';
 
 @Injectable()
 export class AuthService extends NbAuthService {
@@ -12,6 +13,7 @@ export class AuthService extends NbAuthService {
   constructor(
     protected router: Router,
     protected parseService: ParseService,
+    protected strategy: ParseAuthStrategy,
     protected tokenService: NbTokenService,
     @Inject(NB_AUTH_STRATEGIES) protected strategies) {
     super(tokenService, strategies);
@@ -27,12 +29,24 @@ export class AuthService extends NbAuthService {
     const observable$ = super.logout('parse');
 
     // if noone subscribes the observer does not return
-    observable$.pipe(take(1)).subscribe(result => {
-      if (result.isSuccess()) {
+    observable$
+      .pipe(
+        take(1),
+        catchError(e => {
+          console.error(e);
+          const response = e.message || e.toString || e;
+          return of(new NbAuthResult(
+            true,
+            response,
+            this.strategy.getOption(`${module}.redirect.success`)
+          ));
+        })
+      ).subscribe(result => {
         this.tokenService.clear();
-        this.router.navigate([result.getRedirect()]);
-      }
-    });
+        if (result.getRedirect()) {
+          this.router.navigate([result.getRedirect()]);
+        }
+      });
 
     return observable$;
   }
